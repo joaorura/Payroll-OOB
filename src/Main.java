cc11a54f6aebe1b0ee616f0ad67dc1b02f4e7c80
João Messias Lima Pereira <jmlp@ic.ufal.br>
2019-08-06 02:34:47 -0300

    Some translations

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import static interfaces.user.UtilsMain.readEntries;
 import static interfaces.user.Process.processEntries;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Inicializando o sistema\n" +
-                "\tInicializando configurações:\n");
+        System.out.println("Booting the system!\n" +
+                "\tInitializing settings!\n");
         Payroll.getDefault().configurations(UtilsMain.getDate());
 
         int input;
@@ -15,8 +15,8 @@ public class Main {
             input = readEntries(0, 10);
             if (input == 10) return;
             else if (!processEntries(input)) {
-                System.out.println("A sua operação falhou, o sistema não conlcluiu a sua ação. Por favor " +
-                        "realize-a novamente!");
+                System.out.println("Your operation failed, the system does not complete its action. Please" +
+                        "do it again!");
                 return;
             }
         }
